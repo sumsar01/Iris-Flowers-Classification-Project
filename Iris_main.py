b101a6bc82cc31c2671ef5fc97558b80420a5804
sumsar01 <skriver@live.dk>
2020-10-14 15:59:55 +0200

    Plotting data

--- Iris_main.py
+++ Iris_main.py
@@ -13,9 +13,17 @@ sns.set(style='whitegrid', palette='bright',
 #Getting data from SQLite database
 db_file = "./Data/database.sqlite"
 con = sqlite3.connect(db_file)
-df = pd.read_sql_query("SELECT * FROM Iris", con)
+df = pd.read_sql_query("SELECT * FROM Iris", con, index_col="Id")
 con.close()
 
 
 
 print(df.head())
+
+#Species ['Iris-setosa' 'Iris-versicolor' 'Iris-virginica']
+#Plotting data
+ax = sns.jointplot(data=df, x="SepalLengthCm", y="SepalWidthCm", hue="Species", kind="kde")
+ax.set_axis_labels(xlabel='Sepal Length (Cm)', ylabel='Sepal Width (Cm)')
+
+ax = sns.jointplot(data=df, x="PetalLengthCm", y="PetalWidthCm", hue="Species", kind="kde")
+ax.set_axis_labels(xlabel='Petal Length (Cm)', ylabel='Petal Width (Cm)')
