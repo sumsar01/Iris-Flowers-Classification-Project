fe2df09812fe7de057263ffe6e3fa55185e8627a
sumsar01 <skriver@live.dk>
2020-10-15 11:35:42 +0200

    Re-scaling data for NN models

--- Iris_main.py
+++ Iris_main.py
@@ -5,6 +5,7 @@ import matplotlib as plt
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import rcParams
 import sqlite3
+from sklearn.preprocessing import StandardScaler
 
 rcParams['figure.figsize'] = 10,8
 sns.set(style='whitegrid', palette='bright',
@@ -19,7 +20,7 @@ con.close()
 
 
 print(df.head())
-
+"""
 #Species ['Iris-setosa' 'Iris-versicolor' 'Iris-virginica']
 #Plotting data
 ax = sns.jointplot(data=df, x="SepalLengthCm", y="SepalWidthCm", hue="Species", kind="kde")
@@ -27,3 +28,15 @@ ax.set_axis_labels(xlabel='Sepal Length (Cm)', ylabel='Sepal Width (Cm)')
 
 ax = sns.jointplot(data=df, x="PetalLengthCm", y="PetalWidthCm", hue="Species", kind="kde")
 ax.set_axis_labels(xlabel='Petal Length (Cm)', ylabel='Petal Width (Cm)')
+"""
+
+#Re-scaling continuous parameters
+continuous = ['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']
+
+scaler = StandardScaler()
+
+for var in continuous:
+    df[var] = df[var].astype('float64')
+    df[var] = scaler.fit_transform(df[var].values.reshape(-1,1))
+    
+#test-train split
